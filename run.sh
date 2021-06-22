f43622d8943986fece0e8207e110109950646ede
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-06-22 13:20:24 -0700

    save

--- run.sh
+++ run.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+set -eux
 
 rm -rf *.bin a.out *.lock
 
 export UCX_TLS=tcp
 export UCX_WARN_UNUSED_ENV_VARS=n
+
+# cpu
+
+g++ utils.cpp main.cpp alltoall.cpp -std=c++17 -I $UCC_HOME/include -L $UCC_HOME/lib -lucc -lucs -lucp
+
+./a.out 2 0 &
+./a.out 2 1 &
+
+wait
+# rm a.out
+exit
+
+# cuda
 
 nvcc utils.cpp main.cu alltoall.cu -std=c++17 -I $UCC_HOME/include -L $UCC_HOME/lib -lucc -lucs -lucp
 
@@ -13,5 +27,6 @@ nvcc utils.cpp main.cu alltoall.cu -std=c++17 -I $UCC_HOME/include -L $UCC_HOME/
 ./a.out 2 0 &
 ./a.out 2 1 &
 
-wait
 # rm a.out
+
+wait
